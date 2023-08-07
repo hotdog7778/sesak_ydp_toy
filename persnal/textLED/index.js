3ca3c58c92fbf4417151e36a61c4f248154e7a02
김태균 <rlaxorbs159@gmail.com>
2023-08-07 14:02:08 +0900

    textLED: 버튼의 border 처리

--- persnal/textLED/index.js
+++ persnal/textLED/index.js
@@ -1,55 +1,42 @@
 // 화면에 입력값 띄우기
 $('#inputctn').on('input', function (e) {
-        let elem = $(this).val();    
-        $('.led1').val(elem);
-})
+  let elem = $(this).val();
+  $('.led1').val(elem);
+});
 
 // 호버 lev_btns
 
 // 텍스트 속도
 let speedLV = '0s';
-$('.txtspeed').on('click',function() {
-    speedLV = $(this).val();
-    console.log(speedLV);
-})
+$('.tx_speed').on('click', function () {
+  $('.tx_speed').removeClass('border-light');
+  $(this).addClass('border-light');
+  speedLV = $(this).val();
+  $('.led1').css('animationDuration', `${speedLV}`);
+  console.log(speedLV);
+});
+
+console.log($('.tx_size').hasClass('checked')); // false
 
 // 텍스트 크기
 let sizeLV = '100px';
-$('.txtsize').on('click',function() {
-    $(this).toggleClass('border-light');
-    sizeLV = $(this).val();
-    console.log(sizeLV);
-})
+$('.tx_size').on('click', function () {
+  $('.tx_size').removeClass('border-light');
+  $(this).addClass('border-light');
+
+  sizeLV = $(this).val();
+
+  // 미리보기 화면에 폰트사이즈 적용
+  $('.led1').css('fontSize', `${sizeLV}`);
+});
 
 // 시작하기
-$('.runbtn').on('click', function() {
-    $('.led1').css({
-        animationName: 'rolling1',
-        animationDuration: `${speedLV}`,
-        animationIterationCount: 'infinite',
-        animationTimingFunction: 'linear',
-        fontSize: `${sizeLV}`,
-    });
-})
-
-// runBtn.addEventListener("click", function() {
-//     // inputElem.style.animation = "rolling1 3s linear infinite"
-
-//     let roller = document.querySelector('.bannerled');
-//     roller.id = 'roller1';
-//     let clone = roller.cloneNode();
-//     clone.id = 'roller2';
-
-
-    
-//     (document.querySelector('.banner-rap')).append(clone);
-
-//     document.querySelector('#roller1').style.animation = "rolling1 3s linear infinite";
-//     document.querySelector('#roller2').style.animation = "rolling2 3s linear infinite";
-    // document.querySelector('#roller2').style.left = document.querySelector('.roller ul').offsetWidth+'px';
-    
-    // document.getElementById('roller1').style.animation = "3s linear 0s infinite normal forwards running rollingleft1";
-    // document.getElementById('roller2').style.animation = "3s linear 0s infinite normal none running rollingleft2";
-   
-//})
-
+$('.runbtn').on('click', function () {
+  $('.led1').css({
+    animationName: 'rolling1',
+    animationDuration: `${speedLV}`,
+    animationIterationCount: 'infinite',
+    animationTimingFunction: 'linear',
+    fontSize: `${sizeLV}`,
+  });
+});
